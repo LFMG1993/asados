66e54c31a63d69b5f6c8a8f5616cb9389138a3e1
multidevfull <multidevfull@gmail.com>
2025-06-12 04:22:29 +0000

    sliderbar borrado de app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './views/auth/login';
-import SliderBar from './components/dashboard/slider';
 import Home from './views/dashboard/Home';
 import Sales from './views/sale/Sales';
 import { ToastContainer } from 'react-toastify';
@@ -11,12 +10,11 @@ import { ToastContainer } from 'react-toastify';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/SliderBar" element={<SliderBar />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Sales" element={<Sales />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Sales" element={<Sales />} />
+        </Routes>
       <ToastContainer />
     </>
   );
